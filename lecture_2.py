fd0638cae86bf09ec1f761ecae680d6b3aa47fc5
thislife_won <sw8744@naver.com>
2025-03-13 12:21:31 +0900

    Add Lecture 2 and Lecture 3

---
+++ lecture_2.py
@@ -0,0 +1,25 @@
+from cs1robots import *
+
+create_world(avenues=5, streets=5)
+
+# create a robot with one beeper
+hubo = Robot()
+hubo.set_trace("blue")
+
+# move one step forward
+hubo.move()
+
+# turn left 90 degrees
+hubo.turn_left()
+
+def turn_right():
+    hubo.turn_left()
+    hubo.turn_left()
+    hubo.turn_left()
+
+hubo.move()
+turn_right()
+hubo.move()
+hubo.turn_left()
+hubo.move()
+turn_right()
---
+++ lecture_3.py
@@ -0,0 +1,44 @@
+# 간단한 예시
+if True:
+    print("CS101 is my favorite course")
+
+if False:
+    print("Every CS101 student will receive an A+")
+
+if 3 < 5:
+    print("3 is less than 5")
+
+else:
+    print("3 is larger than 5")
+
+# not 연산자 알아보기
+print(not 3 < 5)
+
+from cs1robots import *
+import time
+
+create_world(avenues=3, streets=3)
+hubo = Robot(beepers=20)
+sleep_time = 0.1
+cnt = 0
+
+def dance():
+    for i in range(4):
+        time.sleep(sleep_time)
+        hubo.turn_left()
+
+def move_or_turn():
+    if hubo.front_is_clear():
+        dance()
+        global cnt
+        cnt += 1
+        time.sleep(sleep_time)
+        hubo.move()
+    else:
+        hubo.turn_left()
+        time.sleep(sleep_time)
+        hubo.drop_beeper()
+
+for i in range(20):
+    move_or_turn()
+print(cnt)
